83621a23b47a9fabc82f18609dcc229d6ccf5878
levklon <camacura2018@aol.com>
2024-08-05 04:10:33 +0900

    Initial commit

---
+++ backend/app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} - {self.date}"
